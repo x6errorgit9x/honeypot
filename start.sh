e9b9d965a5d34bea522eba9e6fdcc7f9dbb30ffa
XGotx <gaith12309@gmail.com>
2026-06-16 20:27:40 +0300

    Refactor start.sh to simplify execution flow
    Remove database initialization and honeypot execution from start script.

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-# إنشاء قاعدة البيانات إذا لم تكن موجودة
 python init_db.py
 
-# تشغيل الـ Honeypot في الخلفية
 python honeypot.py &
 
-# تشغيل الـ Flask Dashboard كعملية أساسية
 python dashboard.py
